b9969984afb69ffc77329ec5d6d3a14addfdd27c
Eniola0x <ibitomihieniola007@gmail.com>
2024-12-22 18:57:08 +0100

    intial commit.

---
+++ src/main/java/org/clearstream/account_service/ClearStreamAccountServiceApplication.java
@@ -0,0 +1,13 @@
+package org.clearstream.account_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClearStreamAccountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClearStreamAccountServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/clearstream/account_service/ClearStreamAccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.clearstream.account_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClearStreamAccountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
